abc23e67c19dddba724814f57be678d114fa7cf6
llluqueee <luke73@orange.fr>
2026-02-08 22:23:09 +0100

    Not afk audio playing windows

--- aw_watcher_afk/windows.py
+++ aw_watcher_afk/windows.py
@@ -3,6 +3,9 @@ import time
 from ctypes import POINTER, WINFUNCTYPE, Structure  # type: ignore
 from ctypes.wintypes import BOOL, DWORD, UINT
 
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioMeterInformation
+
 
 class LastInputInfo(Structure):
     _fields_ = [("cbSize", UINT), ("dwTime", DWORD)]
@@ -31,7 +34,31 @@ def seconds_since_last_input():
     return seconds_since_input
 
 
+def is_audio_playing():
+    """Check if any audio is currently playing on the system."""
+    try:
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            if session.Process:
+                volume = session._ctl.QueryInterface(IAudioMeterInformation)
+                if volume.GetPeakValue() > 0.01:
+                    return True
+    except Exception:
+        pass
+    return False
+
+
+def seconds_since_last_input_with_audio():
+    """
+    Returns seconds since last input, but returns 0 if audio is playing.
+    This considers the user as "active" when audio is playing.
+    """
+    if is_audio_playing():
+        return 0.0
+    return seconds_since_last_input()
+
+
 if __name__ == "__main__":
     while True:
         time.sleep(1)
-        print(seconds_since_last_input())
+        seconds_since_last_input_with_audio()
